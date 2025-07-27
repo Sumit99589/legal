092ea7734c3564a819500771395bc2a224cb132d
Sumit Bhatt <sumitbhatt9958@gmail.com>
2025-07-27 21:54:32 +0530

    sidebar fix

--- src/components/ui/appsidebar.tsx
+++ src/components/ui/appsidebar.tsx
@@ -1,6 +1,18 @@
-import { Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarHeader, SidebarMenu, SidebarMenuBadge, SidebarMenuButton, SidebarMenuItem } from "@/components/ui/sidebar"
-import { Calendar, Home, Inbox, Search, Settings } from "lucide-react"
-import React, {ReactNode} from "react";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuBadge,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import { Home, Inbox } from "lucide-react";
+import React, { ReactNode } from "react";
 
 const items = [
   {
@@ -12,45 +24,45 @@ const items = [
     title: "Ask Bot",
     url: "/bot",
     icon: Inbox,
-  }
-]
+  },
+];
 
 interface AppsidebarProps {
-  children: ReactNode;
+  children?: ReactNode;
 }
 
-export default function Appsidebar({children}: AppsidebarProps){
-    return(
-        <div>
-            <Sidebar >
-                <SidebarHeader className="text-2xl mx-auto mb-3.5 font-medium">
-                    LegalDoc
-                </SidebarHeader>
-                <SidebarContent>
-                    <SidebarGroup>
-                        <SidebarGroupLabel className="text-xl">Application</SidebarGroupLabel>
-                        <SidebarGroupContent>
-                            <SidebarMenu>
-                                {items.map((item) => (
-                                    <SidebarMenuItem key={item.title}>
-                                      <SidebarMenuButton asChild>
-                                        <a href={item.url}>
-                                          <item.icon />
-                                          <span className="text-sm font-medium">{item.title}</span>
-                                        </a>
-                                      </SidebarMenuButton>
-                                    </SidebarMenuItem>
-                                ))} 
-                            </SidebarMenu>
-                        </SidebarGroupContent>
-                    </SidebarGroup>
-                </SidebarContent>
-                <SidebarFooter className="items-center">
-                    <h1>© 2025 LegalDoc.</h1>
-                    <h1> All rights reserved.</h1>
-                </SidebarFooter>
-            </Sidebar>
-            {children}
-        </div>
-    )
+export default function Appsidebar({ children }: AppsidebarProps) {
+  return (
+    <div>
+      <Sidebar>
+        <SidebarHeader className="text-2xl mx-auto mb-3.5 font-medium">
+          LegalDoc
+        </SidebarHeader>
+        <SidebarContent>
+          <SidebarGroup>
+            <SidebarGroupLabel className="text-xl">Application</SidebarGroupLabel>
+            <SidebarGroupContent>
+              <SidebarMenu>
+                {items.map((item) => (
+                  <SidebarMenuItem key={item.title}>
+                    <SidebarMenuButton asChild>
+                      <a href={item.url}>
+                        <item.icon />
+                        <span className="text-sm font-medium">{item.title}</span>
+                      </a>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))}
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        </SidebarContent>
+        <SidebarFooter className="items-center">
+          <h1>© 2025 LegalDoc.</h1>
+          <h1>All rights reserved.</h1>
+        </SidebarFooter>
+      </Sidebar>
+      {children}
+    </div>
+  );
 }
